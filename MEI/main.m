7aad2d5b5730dfe2811eb88715f641d3119447a1
Yosemite <Yosemite@Yosemite.local>
2015-03-05 11:01:34 +0800

    Initial Commit

---
+++ MEI/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MEI
+//
+//  Created by Yosemite on 3/5/15.
+//  Copyright (c) 2015 same.world. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SAppDelegate class]));
+    }
+}
